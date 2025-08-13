c322fd214fbe81260b076c00451ef857e58a6b67
patnala sri krishna sai <96357392+krishkrishna03@users.noreply.github.com>
2025-08-13 21:55:08 +0530

    Delete app directory

--- app/embedder.py
+++
@@ -1,6 +0,0 @@
-from sentence_transformers import SentenceTransformer
-
-model = SentenceTransformer('all-MiniLM-L6-v2')
-
-def embed_text(texts):
-    return model.encode(texts, show_progress_bar=True)
